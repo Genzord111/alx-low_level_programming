53065fa5f5999459b29a4f2b5764fc76d0db2316
solomon felicien <feliciensolomon5@gmail.com>
2023-10-06 17:01:47 -0700

    first 2 advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Code displays combination of two digits'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int start = 0;
+int end = 10;
+int i;
+int j;
+for (i = start; i < end; i++)
+{
+
+	for (j = start; j < end; j++)
+
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (!(i == 9 && j == 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Code displays differnt combination of 3 digits'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int start = 0;
+int end = 10;
+int i;
+int j;
+int k;
+int lower_counter = 3;
+int upper_counter = 10;
+for (i = start; i < end; i++)
+{
+	for (j = start; j < end; j++)
+	{
+		for (k = start; k < end; k++)
+		{
+			if (j < k && i < j)
+			{
+				if (!(i == j || i == k || j == k))
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (!(i == 9 && j == 9 && k == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
