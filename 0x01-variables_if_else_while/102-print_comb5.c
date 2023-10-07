20bb0699564a83dc47353ff0b4006c6a1562ef89
solomon felicien <feliciensolomon5@gmail.com>
2023-10-06 18:26:59 -0700

    will continue later

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Code displays combination of two digits'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int start = 0;
+int end = 10;
+int i;
+int j;
+int x;
+int y;
+int counter = 1;
+for (x = start; x < end; x++)
+{
+	for (x = start; x < end; x++)
+	{
+		putchar(x + '0');
+		putchar(y + '0');
+		putchar(' ');
+		for (i = start; i < end; i++)
+		{
+			for (j = start; j < end; j++)
+			{
+				if (!(i == 9 && j == 9 && x == 9 && y == 8))
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(',');
+				}
+				else
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
