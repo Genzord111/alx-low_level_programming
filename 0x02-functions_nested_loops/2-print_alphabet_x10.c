c010008347979f60e05de58c7815701701ea5e97
solomon felicien <feliciensolomon5@gmail.com>
2023-10-10 09:05:09 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints a to z ten 10times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char start = 'a';
+	char end = 'z';
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = start; i <= end; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+	}
+}
