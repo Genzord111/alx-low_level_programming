39b6c1fe0884bb415bd2b45e8192f4a680b6e418
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2023-10-11 00:38:08 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,24 +12,28 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d ", i);
+			printf("%d", i);
 			if (i < 98)
 			{
 				printf(",");
 				printf(" ");
 			}
+			else
+				printf(" ");
 		}
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d ", i);
+			printf("%d", i);
 			if (i > 98)
 			{
 				printf(",");
 				printf(" ");
 			}
+			else
+				printf(" ");
 		}
 	}
 	else
