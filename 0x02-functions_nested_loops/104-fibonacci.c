9556b2a981dc04b8e7d340e953cd4f837255de81
solomon felicien <feliciensolomon5@gmail.com>
2023-10-11 15:11:55 -0700

    updates

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Prints first 98 fibonacci numbers'
+* @void: takes no parameters
+* Return: returns 0 (Success)
+*/
+
+int main(void)
+{
+	long int i;
+	long int num2 = 2;
+	long int num1 = 1;
+	long int result;
+
+	printf("1, 2, ");
+	for (i = 1; i <= 96; i++)
+	{
+		result = num2 + num1;
+		num1 = num2;
+		num2 = result;
+		printf("%ld", result);
+		if (i < 96)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
