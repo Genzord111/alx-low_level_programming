04533b055c346b4d3fd8d63fe844bda6c332b441
solomon felicien <feliciensolomon5@gmail.com>
2023-10-11 14:56:26 -0700

    updates

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-        long int i;
-        long int num2 = 2;
-        long int num1 = 1;
-        long int result = 0;
-        long int total = num2;
-	
+	long int i;
+	long int num2 = 2;
+	long int num1 = 1;
+	long int result = 0;
+	long int total = num2;
+
 	for (i = 1; i <= 48; i++)
 	{
 		if (result <= 4000000)
@@ -22,7 +22,7 @@ int main(void)
 			result = num2 + num1;
 			num1 = num2;
 			num2 = result;
-			if(result % 2 == 0)
+			if (result % 2 == 0)
 			{
 				total = total + result;
 			}
