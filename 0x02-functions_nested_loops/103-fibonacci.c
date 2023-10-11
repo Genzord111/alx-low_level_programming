69e5a2a67bd76fad81692a440b0a0e9c95dc5b31
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2023-10-11 21:47:07 +0000

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
-* Description: 'Prints first 50 fibonacci numbers'
+* Description: 'Prints the sum of even fibonacci numbers
 * @void: takes no parameters
 * Return: returns 0 (Success)
 */
 
 int main(void)
 {
-	long int i;
-	long int num2 = 2;
-	long int num1 = 1;
-	long int result;
-
-	printf("1, 2, ");
+        long int i;
+        long int num2 = 2;
+        long int num1 = 1;
+        long int result = 0;
+        long int total = num2;
+	
 	for (i = 1; i <= 48; i++)
 	{
 		if (result <= 4000000)
 		{
-			if(num1 % 2 == 0 && num2 % 2 == 0)
+			result = num2 + num1;
+			num1 = num2;
+			num2 = result;
+			if(result % 2 == 0)
 			{
-				result = num2 + num1;
-				num1 = num2;
-				num2 = result;
-				printf("%ld", result);
-			}
-			if (i < 48)
-			{
-				putchar(',');
-				putchar(' ');
+				total = total + result;
 			}
 		}
 	}
+	printf("%ld", total);
 	putchar('\n');
 	return (0);
 }
