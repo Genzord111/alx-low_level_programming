6e9217a00ad7b941e38b391d6ddecc9ce7584264
solomon felicien <feliciensolomon5@gmail.com>
2023-10-12 15:11:39 -0700

    update to void print_triangle(int size)

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+/**
+* print_space - prints the space before the '#'
+* @end: integer that accounts for the spaces
+* Return: void
+*/
+
+void print_space(int end)
+{
+	int j;
+
+	if (end > 1)
+	{
+		for (j = 1; j < end; j++)
+		{
+			putchar(' ');
+		}
+	}
+}
+
+/**
+* print_hash - prints the '#'
+* @start: integer that accounts for number of '#'
+* Return: void
+*/
+void print_hash(int start)
+{
+	int j;
+
+	for (j = 1; j < start; j++)
+	{
+		putchar('#');
+	}
+}
+
+/**
+* print_diagonal - draws a right angle triangle
+* @n: integer that accounts for the height of diagonal
+* Return: void
+*/
+void print_triangle(int size)
+{
+	int i;
+	int n;
+
+	if (size >= 1)
+	{
+		n = size;
+		for (i = 1; i <= size; i++)
+		{
+			if (size > 0)
+			{
+				print_space(n);
+				n--;
+				print_hash(i);
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+	else
+		putchar('\n');
+}
