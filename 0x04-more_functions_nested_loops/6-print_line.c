1f93c6756f66f2be35c82ad2e5408dc0729e795a
solomon felicien <feliciensolomon5@gmail.com>
2023-10-12 06:24:29 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* more_numbers - draws a line
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int n = 3;
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
