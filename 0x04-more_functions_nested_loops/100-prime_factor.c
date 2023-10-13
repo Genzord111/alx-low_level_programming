699c9b17cb083f1c566879ef079800fa53693581
solomon felicien <feliciensolomon5@gmail.com>
2023-10-13 14:15:45 -0700

    added code for 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point for program
+* @void - takes no parameter
+* Return: returns 0 if (SUCCESS)
+*/
+int main(void)
+{
+	long long int i = 2;
+	long long int num = 612852475143;
+	long long int value = num;
+
+	while (num / i != 1)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+		}
+		else
+			i++;
+	}
+	printf("%lld\n", num);
+}
