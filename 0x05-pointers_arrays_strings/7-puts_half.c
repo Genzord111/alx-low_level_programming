6f9520ad1b1b31c99aca6c54a1dfc54dee249cac
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2023-10-17 00:23:09 +0000

    Update 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+/**
+* puts_half - prints half the index values in a string
+* @str: is a pointer to an string variable
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+  size_t i;
+  size_t start;
+  char p3[100];
+
+  strcpy(p3, str);
+  if ( strlen(p3) % 2 == 0)
+  {
+    start = strlen(p3) / 2;
+  }
+  else
+    start = (strlen(p3) -1) / 2;
+
+  for (i = start; i < strlen(p3); i++)
+  {
+    _putchar(p3[i]);
+  }
+  _putchar('\n');
+}
