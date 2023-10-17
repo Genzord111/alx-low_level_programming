7025387809f0fc721c887216de3e427f3285f857
solomon felicien <feliciensolomon5@gmail.com>
2023-10-16 17:41:43 -0700

    update to file 7 and 8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include <string.h>
-#include "main.h"
 /**
 * puts_half - prints half the index values in a string
 * @str: is a pointer to an string variable
@@ -8,23 +8,23 @@
 
 void puts_half(char *str)
 {
-  size_t i;
-  size_t start;
-  char p3[500];
+	size_t i;
+	size_t start;
+	char p3[100];
 
-  strcpy(p3, str);
-  if ( strlen(p3) % 2 == 0)
-  {
-    start = strlen(p3) / 2;
-  }
-  else
-  {
-    start = (strlen(p3) -1) / 2;
-    start =start + 1;
-  }
-  for (i = start; i < strlen(p3); i++)
-  {
-    _putchar(p3[i]);
-  }
-  _putchar('\n');
+	strcpy(p3, str);
+	if (strlen(p3) % 2 == 0)
+	{
+		start = strlen(p3) / 2;
+	}
+	else
+	{
+		start = (strlen(p3) - 1) / 2;
+		start = start + 1;
+	}
+	for (i = start; i < strlen(p3); i++)
+	{
+		putchar(p3[i]);
+	}
+	putchar('\n');
 }
