c9c473202bbef41f7cc118c69c136b1de31816ec
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2023-10-17 00:24:51 +0000

    Update 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_array - prints the n indexes of an array
+* @n: is a pointer to an integer variable
+* @a: is a pointer to an integer array
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+  int i;
+
+  for (i = 0; i < n; i++)
+  {
+    printf("%d", a[i]);
+    if( i < n -1)
+    {
+      printf(", ");
+    }
+  }
+  putchar('\n');
+}
