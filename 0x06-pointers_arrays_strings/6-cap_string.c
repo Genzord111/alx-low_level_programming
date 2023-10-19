dccd9c8a0009e8a97d8bebe14490c9bc0b5034a6
solomon felicien <feliciensolomon5@gmail.com>
2023-10-19 13:45:54 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,20 +3,62 @@
 #include <stdio.h>
 
 /**
-* string_toupper - changes all text to uppercase
-* @str: is a pointer to an string
-* Return: string
+* check - checks if parameter is an escape character
+* @str: charcter variable to be checked
+* Return: 0 if (SUCCESS)
 */
-char *string_toupper(char *str)
+
+int check(char str)
+{
+	switch (str)
+	{
+		case ' ':
+			return (0);
+		case ',':
+			return (0);
+		case '.':
+			return (0);
+		case '{':
+			return (0);
+		case '}':
+			return (0);
+		case '?':
+			return (0);
+		case '!':
+			return (0);
+		case '(':
+			return (0);
+		case ')':
+			return (0);
+		case '\t':
+			return (0);
+		case '\n':
+			return (0);
+		case '"':
+			return (0);
+		case ';':
+			return (0);
+	}
+}
+
+/**
+* cap_string - checks if parameter is an escape character
+* @str: string variable to be checked
+* Return: str string
+*/
+
+char *cap_string(char *str)
 {
 	int i;
+	int size = (int)strlen(str);
 
-	for (i = 0; i < (int)strlen(str); i++)
+	if ((int)str[0] > 96 && (int)str[0] < 123)
+		str[0] = (int)str[0] - 32;
+
+	for (i = 1; i < size; i++)
 	{
-		if ((int)str[i] > 96 && (int)str[i] < 123)
-		{
+		if (i > 0 && check(str[i - 1]) == 0 && (str[i] > 96 && str[i] < 123))
 			str[i] = (int)str[i] - 32;
-		}
 	}
 	return (str);
 }
