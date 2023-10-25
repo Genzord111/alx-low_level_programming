fb2df24712affdb5b8aa322dae347b4cb08cb83a
solomon felicien <feliciensolomon5@gmail.com>
2023-10-25 10:51:09 -0700

    first batch

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*  _strlen_recursion - prints length of a string
+* @s: is a pointer to an integer variable
+* Return: void
+*/
+
+void _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
