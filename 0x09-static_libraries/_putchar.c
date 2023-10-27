5bc2a70c8379082c34ce2d9ec9429ba20b1b842e
solomon felicien <feliciensolomon5@gmail.com>
2023-10-27 11:06:57 -0700

    update

--- 0x09-static_libraries/_putchar.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h> // Include the necessary header for write() function
-
-// Declaration of _putchar function
-int _putchar(char c);
-
-int _putchar(char c) {
-    return write(1, &c, 1); // 1 represents stdout (standard output)
-}
--- 0x09-static_libraries/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for file in *.c; do gcc -c "$file"; done
