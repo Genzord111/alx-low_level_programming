c90dd9a6e28edfdfc177d0521593925742806199
Genzord111 <feliciensolomon5@gmail.com>
2024-09-19 15:07:46 -0700

    m

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,39 +1,33 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* add_dnodeint - adds new node to begining of a linked list
-* @head: pointer to current node
-* @n: num to be duplicated in current node
-* Return: pointer to head
-*/
-
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list
+ * @head: Pointer to the pointer of the head of the list
+ * @n: Value to be added to the new node
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *former;
 
-	if (*head != NULL)
-	{
-
-		while ((*head)->prev != NULL)
-		{
-			*head = (*head)->prev;
-
-		}
-	}
-
-	former = *head;
+	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
+	/* Initialize the new node */
 	new_node->n = n;
-	new_node->next = former;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	/* If the list is not empty, update the prev pointer of the current head */
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
 
 	*head = new_node;
+
 	return (new_node);
 }
