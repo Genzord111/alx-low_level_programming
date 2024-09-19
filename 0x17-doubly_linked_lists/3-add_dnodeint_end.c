f5a565a2298bd348cdb7dad8fe35974d3ed1df97
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-09-19 22:25:40 +0000

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,16 +10,16 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
     dlistint_t *new_node, *last;
-
+    
     /* Allocate memory for the new node */
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
         return (NULL);
-
+    
     /* Initialize the new node */
     new_node->n = n;
     new_node->next = NULL;
-
+    
     /* If the list is empty, make the new node the head */
     if (*head == NULL)
     {
@@ -27,7 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         *head = new_node;
         return (new_node);
     }
-
+    
     /* Traverse to the last node */
     last = *head;
     while (last->next != NULL)
@@ -36,6 +36,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
     /* Connect the new node to the end of the list */
     last->next = new_node;
     new_node->prev = last;
-
+    
     return (new_node);
 }
