1c4a2f11e58fbe9c4e78525200ee6e21733e2766
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-12 12:59:46 +0000

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -25,7 +25,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			return(strdup(current->value));
+			return (strdup(current->value));
 		}
 
 		current = current->next;
