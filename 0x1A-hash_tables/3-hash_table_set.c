d9f6bc61bbded907679501a7d5876832d0159a0b
Genzord111 <feliciensolomon5@gmail.com>
2024-10-14 04:30:36 -0700

    reduced 3*.c to 3 functions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,17 +13,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *current_node;
-	char *dup_key = strdup(key);
-	char *dup_value = strdup(value);
+	hash_node_t *current_node;
+	char *dup_key, *dup_value;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (0);
-
+	dup_key = strdup(key);
+	dup_value = strdup(value);
 	if (dup_key == NULL || dup_value == NULL)
 	{
 		free(dup_key);
@@ -31,30 +28,70 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	current_node = ht->array[index];
-	while (current_node != NULL)
-	{
-		if (strcmp(current_node->key, key) == 0)
-		{
-			free(current_node->value);
-			current_node->value = dup_value;
-			free(dup_key);
-			return (1);
-		}
-		current_node = current_node->next;
-	}
+	index = key_index((const unsigned char *)key, ht->size);
 
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
+	if (index >= ht->size)
 	{
 		free(dup_key);
 		free(dup_value);
 		return (0);
 	}
 
-	new_node->key = dup_key;
-	new_node->value = dup_value;
+	current_node = ht->array[index];
+
+	if (update_existing_node(current_node, key, dup_value) == 1)
+	{
+		free(dup_key);
+		return (1);
+	}
+
+	return (insert(ht, index, dup_key, dup_value));
+}
+
+/**
+ * update_existing_node - Updates the value of an existing node
+ * @node: The current node in the hash table
+ * @key: The key for the element
+ * @new_value: The new value for the element
+ *
+ * Return: 1 if the node is updated, 0 otherwise
+ */
+int update_existing_node(hash_node_t *node, const char *key, char *new_value)
+{
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = new_value;
+			return (1);
+		}
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
+ * insert - Inserts a new node into the hash table
+ * @ht: The hash table
+ * @index: The index to insert the node at
+ * @key: The key for the new node
+ * @value: The value for the new node
+ * Return: 1 if the node is inserted, 0 otherwise
+ */
+int insert(hash_table_t *ht, unsigned long int index, char *key, char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+	{
+		free(key);
+		free(value);
+		return (0);
+	}
+
+	new_node->key = key;
+	new_node->value = value;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,4 +37,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+int update_existing_node(hash_node_t *node, const char *key, char *value);
+int insert(hash_table_t *ht, unsigned long int index, char *key, char *value);
 #endif
